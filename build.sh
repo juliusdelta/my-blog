e8fe59b6e94cdc2fef51dce7dfa28a3d404c2a6a
JD Gonzales <jd_gonzales@icloud.com>
2025-09-20 22:27:06 -0500

    Build updates

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 echo "Building hugo..."
 
+git submodule update --init
+
 hugo 2>&1
 echo "Hugo site built!"
 
-echo "Copying _redirects..."
-cp _redirects public/
-echo "Redirects copied!"
+# echo "Copying _redirects..."
+# cp _redirects public/
+# echo "Redirects copied!"
 
 echo "Build successful"
